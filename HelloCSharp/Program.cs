925997a378955cbabdc0969ad3e26908ee3dcd2f
RayzaAnchayhua <93015921+RayzaAnchayhua@users.noreply.github.com>
2024-04-15 16:48:32 -0300

    Update2.0

--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -1,4 +1,4 @@
 ﻿string input = "123";
 int number = int.TryParse(input, out number) ? number : 0;
 
-Console.WriteLine($"O valor convertido é: {number}");
+Console.WriteLine($"O valor vira: {number}");
